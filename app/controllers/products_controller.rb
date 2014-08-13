d2bd8989ad444dfadfcdad82491abb7e85fea07d
Philip Bradley <pdbradley@gmail.com>
2014-08-13 14:10:48 -0400

    more cosmetic touches

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,7 +42,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to edit_product_path(@product), notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
@@ -69,7 +69,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      binding.pry
       params.require(:product).permit(:name, :quantity, materials_attributes: [:id, :name, :weight, :_destroy])
     end
 end
